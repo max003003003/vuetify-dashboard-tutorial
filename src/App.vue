a69046d84916802420d8499ec12829f027650f2a
vorabhol <vorabhol.ch@tnis.com>
2019-08-10 14:15:01 +0700

    Add navbar item

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+   <Navbar></Navbar>
   <v-content>
     <router-view></router-view>
   </v-content>
@@ -7,11 +8,11 @@
 </template>
 
 <script>
-
+import Navbar from '@/components/Navbar'
 export default {
   name: 'App',
+  components: { Navbar },
   data: () => ({
-    //
   })
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="home">
   <h1>HomePage</h1>
-   <v-btn class="hidden-md-and-down">Medium and down</v-btn>
-   <v-btn class="hidden-md-and-up">Medium and up</v-btn>
-   <v-btn class="hidden-sm-only">This is Small creen</v-btn>
+
   </div>
 </template>
 
