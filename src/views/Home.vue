5b03099b7534b4c651d59e99b975ef7c849558ca
vorabhol <vorabhol.ch@tnis.com>
2019-08-10 13:44:45 +0700

    Button vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,22 @@
     <v-btn flat color="pink">Click</v-btn>
 
     <v-btn depressed class="pink white--text">
-        <v-icon>email</v-icon>
+        <v-icon left>email</v-icon>
+        <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed small class="pink white--text">
+        <v-icon left small >email</v-icon>
+        <span>email me</span>
+    </v-btn>
+
+     <v-btn depressed large class="pink white--text">
+        <v-icon left large>email</v-icon>
+        <span>email me</span>
+    </v-btn>
+
+   <v-btn depressed  small dark color="pink">
+        <v-icon left>favorite</v-icon>
     </v-btn>
 
   </div>
