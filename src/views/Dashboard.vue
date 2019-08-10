453d8cd3d6a6b44af2cd67c5c75311f78f65769f
vorabhol <vorabhol.ch@tnis.com>
2019-08-10 21:43:24 +0700

    Move element

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,53 +1,44 @@
 <template>
-  <div class="home">
-  <h1 class="subheading gre--text">HomePage</h1>
-<v-container class="my-5">
-     <v-layout row wrap>
-
-  <v-flex xs12 md6>
-         <v-btn outline block class="primary">1</v-btn>
-  </v-flex>
-  <v-flex xs4 md2>
-         <v-btn outline block class="primary">2</v-btn>
-  </v-flex>
-  <v-flex xs4 md2>
-         <v-btn outline block class="primary">3</v-btn>
-  </v-flex>
-<v-flex xs4 md2>
-         <v-btn outline block class="primary">4</v-btn>
-  </v-flex>
-
-     </v-layout>
-
-     <v-layout row wrap>
-  <v-flex xs12 md6>
-    <v-btn outline block class="primary">1</v-btn>
-  </v-flex>
-   <v-flex xs4 md2>
-    <v-btn outline block class="primary">2</v-btn>
-  </v-flex>
-  <v-flex xs4 md2>
-    <v-btn outline block class="primary">3</v-btn>
-  </v-flex>
-
- <v-flex xs4 md2>
-    <v-btn outline block class="primary">4</v-btn>
-  </v-flex>
-
-     </v-layout>
-
-<v-layout row wrap justify-space-around>
- <v-flex xs4 md3>
-    <v-btn outline block class="success">1</v-btn>
-  </v-flex>
- <v-flex xs4 md3>
-    <v-btn outline block class="success">2</v-btn>
-  </v-flex>
-
-
-</v-layout>
-   </v-container>
-
+  <div class="dashboard">
+  <h1 class="subheading grey--text">Dashboard</h1>
+  <v-container class="my-5">
+       <v-card flat class="pa-6">
+         <v-layout row wrap>
+           <v-flex xs12 md6>
+              <div class="caption grey--text">
+                 Project title
+              </div>
+             <div>
+                 Create a new website
+              </div>
+           </v-flex>
+           <v-flex xs6 sm4 md2>
+              <div class="caption grey--text">
+                Project title
+              </div>
+              <div>
+                The new untitled
+              </div>
+           </v-flex>
+           <v-flex xs6 sm4 md2>
+              <div class="caption grey--text">
+                Duedate
+              </div>
+              <div>
+                1st Jan 2019
+              </div>
+           </v-flex>
+           <v-flex xs6 sm4 md2>
+              <div class="caption grey--text">
+                status
+              </div>
+              <div>
+              ongoing
+            </div>
+           </v-flex>
+         </v-layout>
+       </v-card>
+  </v-container>
   </div>
 </template>
 
